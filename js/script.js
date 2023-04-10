71746cd8eee93fe9f58de8261a28e9f9f60e148f
Manuel <ma.figueroap@duocuc.cl>
2023-04-09 21:20:10 -0400

    Agregado  de producto en teclado, mas funcion js para el carrito

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var contador = document.getElementById("productos");
+var precio = parseFloat(document.getElementById("precio").textContent) 
+var total = document.getElementById("total");
+
+var cont = 0;
+var tot = 0;
+
+function añadir()
+{
+    cont += 1;
+    contador.innerText = cont;
+
+    tot = precio + parseFloat(total.textContent); 
+
+    total.textContent = tot;
+
+}
