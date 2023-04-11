3b4be90c34f09995819c87a43e1d1e6337b4cade
ShiaggyDuoc <ke.carvajalv@duocuc.cl>
2023-04-11 17:36:21 -0400

    maldito carrito

--- js/script.js
+++ js/script.js
@@ -2,18 +2,30 @@ var contador = document.getElementById("productos");
 var precio = parseFloat(document.getElementById("precio").textContent);
 var total = document.getElementById("total");
 
-var cont = 0;
-var tot = 0;
+var datosDelCarrito = JSON.parse(localStorage.getItem("carrito")) || { cont: 0, tot: 0 };
 
-function añadir() {
-  cont += 1;
+var cont = datosDelCarrito.cont;
+var tot = datosDelCarrito.tot;
+
+function actualizarCarrito() {
   contador.innerText = cont;
-
-  tot = precio + parseFloat(total.textContent);
-
   total.textContent = tot.toFixed(3);
 }
 
+function añadir() {
+  cont += 1;
+  tot = precio + parseFloat(total.textContent);
 
+  localStorage.setItem("carrito", JSON.stringify({ cont: cont, tot: tot }));
 
-  
+  actualizarCarrito();
+}
+
+actualizarCarrito();
+
+function vaciarCarrito() {
+  cont = 0;
+  tot = 0;
+  localStorage.removeItem("carrito");
+  actualizarCarrito();
+}
