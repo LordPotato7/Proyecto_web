61a02077857ec701494c29583de78c28e9b1f221
Manuel <ma.figueroap@duocuc.cl>
2023-05-03 11:25:00 -0400

    Agregado de validacion de form (falta el del pago)

--- js/v_crearUsuarioa.js
+++ js/v_crearUsuarioa.js
@@ -15,11 +15,31 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById("alertaN").textContent = "El nombre no es suficientemente largo";
         return;
       }
+      
+	  var emailField = document.getElementById('email');
+	  var validEmail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+    
+    if (!validEmail.test(emailField.value)){
+      document.getElementById("alertaE").textContent = "La email no es válida";
+      return;
+
+    }
+
     var clave = document.getElementById('password').value;
     if (clave.length <= 6) {
-      
       document.getElementById("alertaP").textContent = "La clave no es válida";
       return;
     }
+
+    var fileInput = document.getElementById("imagen");
+    var file = fileInput.files[0];
+
+    if(!file)
+    {
+        document.getElementById("alertaI").textContent = "no ha seleccionado una imagen";
+        return;
+    }
     this.submit();
   }
+
+ 
